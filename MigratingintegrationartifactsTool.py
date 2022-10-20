020b569ee2d19010cf8e3e9b8ea8c7ecc352bdf4
Omar Salah <somar3296@gmail.com>
2022-10-20 13:33:19 +0200

    Update MigratingintegrationartifactsTool.py

--- MigratingintegrationartifactsTool.py
+++ MigratingintegrationartifactsTool.py
@@ -4,7 +4,7 @@ userpath = input("Enter path for project artifacts(i.e the src folder path C:/bl
 print(userpath)
 basepath="/src/main/synapse-config/"
 subfolders=["api","endpoints","inbound-endpoints","local-entries","message-processors","message-stores","proxy-services","sequences","tasks","templates"]
-def read_text_file(file_path):
+def read_text_file(file_path,filename):
     file1 = open(file_path, 'r')
     count = 0
     while True:
@@ -21,7 +21,7 @@ def read_text_file(file_path):
             while "<args"not in line2: #keep searching until you reach <args> tag
                 if "$ctx:" in line2:
                     #ctx found inside body and need to be changed
-                    print("review file ",file_path," as it contains $ctx in a payloadfactory body in line: ",count)
+                    print("review file ",filename," as it contains $ctx in a payloadfactory body in line: ",count)
                 line2=file1.readline()
                 count+=1        
     file1.close()
@@ -40,7 +40,7 @@ for subfolderpath in subfolders:
         if file.endswith(".xml"):
             file_path = f"{userpath+basepath+subfolderpath}\{file}"
             # call read text file function
-            read_text_file(file_path)
+            read_text_file(file_path,file)
     print("done checking folder: ",subfolderpath)
   
   
